40d82329d7f7bf709404b54bcc7026bf11aa007d
Leandro Carrasco <leandro.carrasco@travelgenio.com>
2021-09-26 22:37:32 -0300

    Se agrega cache

--- src/main/java/com/leacar21/technical/theater/catalog/services/impl/TheaterCatalogServiceImpl.java
+++ src/main/java/com/leacar21/technical/theater/catalog/services/impl/TheaterCatalogServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +29,7 @@ public class TheaterCatalogServiceImpl implements TheaterCatalogService {
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable(value = "theaters", unless = "#result.size() == 0")
     public List<TheaterDTO> getAll(Boolean enable) {
         List<Theater> listTheater = null;
         if (enable == null) {
