a27e6c7c0402e55299de7c78460f224329357831
Leandro Carrasco <leandro.carrasco@travelgenio.com>
2021-10-16 13:26:59 -0300

    Dix en SpringFoxConfig

--- src/main/java/com/leacar21/technical/theater/catalog/config/SpringFoxConfig.java
+++ src/main/java/com/leacar21/technical/theater/catalog/config/SpringFoxConfig.java
@@ -15,9 +15,10 @@ public class SpringFoxConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                                                      .apis(RequestHandlerSelectors.any())
-                                                      .paths(PathSelectors.ant("/" + TheaterCatalogController.THEATERS_RESOURCE))
-                                                      .build();
+        var docket = new Docket(DocumentationType.SWAGGER_2);
+        return docket.select()
+                     .apis(RequestHandlerSelectors.any())
+                     .paths(PathSelectors.ant("/" + TheaterCatalogController.THEATERS_RESOURCE))
+                     .build();
     }
 }
